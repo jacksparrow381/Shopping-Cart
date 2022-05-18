d676cb84092062b4ab31df4cc009ba0a08c9b2fe
jackSparrow381 <developerasad381@gmail.com>
2022-05-18 11:29:35 +0500

    Created Product Section and set functionality for showing the products

--- src/components/Section/productsection.js
+++ src/components/Section/productsection.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 
 export default function ProductSection(props) {
   const { products, handleAddProduct } = props;
+
+  // showing the products
   return (
     <Box>
       <Box className="product-list">
