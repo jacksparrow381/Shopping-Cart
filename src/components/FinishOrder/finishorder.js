5a173d74d58049ff8d29654e4e408d3093caab7e
jackSparrow381 <developerasad381@gmail.com>
2022-05-18 11:35:40 +0500

    finish page when user purchased the product

--- src/components/FinishOrder/finishorder.js
+++ src/components/FinishOrder/finishorder.js
@@ -2,6 +2,8 @@ import { Box, Button, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import "./finishorder.css";
 
+// when user clicks on finish order button, it will redirect to order confirmation page
+
 export function FinishOrder() {
   return (
     <Box className="finish-order">
